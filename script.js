c1943949fb773f74837e42ad28ba5fe96661330d
AnisghebacheCFC <anisghebache@googlemail.com>
2024-06-18 14:12:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2278,7 +2278,6 @@ function displayLists(insurance) {
         "Media": ["Advertising Agency", "Artist", "Author", "Blogger / Vlogger", "Broadcaster", "Communication Consultants", "Digital Marketing", "Event organisers", "Film and TV Distributor", "Film and TV Production", "Magazine, Book, Newspaper Publishers", "Marketing", "Marketing and PR Companies", "Marketing Consultants", "Multimedia Companies", "Music Producers / Publishers / Composers / Songwriters", "New Media / digital agencies", "Photographers", "PR Consultants", "Press agencies", "Producers (TV, Film, Commercials)", "Public Relations", "Radio Stations", "Social Media Agencies"],
         "Professions": ["Agricultural consultants", "Business consultants", "Captives", "Civil Engineers", "Commercial lines insurance", "Conservation managers", "Construction", "Conveyancers", "Data processors", "Directional drillers", "Drilling consultants", "Driving / Haulage", "Education", "Electrical Engineers", "Engineering and technical", "Environmental Engineers", "Environmental consultants", "Estate agents (residential and commercial)", "Expert witnesses", "Feasibility studies", "Financial services", "Fire consultants", "Food consultants", "Foreclosure agents", "Geologists", "Geotechnical engineers", "Health and safety consultants", "Hospitality", "HR consultants", "HVAC engineers", "Hydraulic / fire engineers", "Interim Management", "Interior designers", "Investigators", "IT / Telecommunications", "Land brokers", "Land surveyors", "Landscape architects", "Management consultants", "Mechanical engineers", "Other professional services", "Personal lines", "Project managers", "Property developers", "Property leasing firms", "Property management companies", "Property management firms", "Property surveyors", "Quality assurance consultants", "Quarterly surveyors", "Recruitment", "Risk retention groups", "Staffing", "Structural architects / engineers / surveyors", "Town planning engineers", "Training companies"],
         "Tech": ["App developers", "Application service providers", "Bespoke software developers", "Business networking websites", "Computer games developers", "Corporate blogs", "Digital Marketing agencies", "Domain name registration (resellers)", "Educational games", "Email providers", "Encryption software providers", "Genealogy websites", "Hardware value added resellers", "Instant messaging applications", "Internet radio websites", "IT consultancy", "IT security consultants", "IT support and training", "Mobile content providers", "Mobile phone content developers", "Networking engineers", "Online dating agencies", "Online gaming providers", "Online games developers and publishers", "Online listings sites", "Packaged software developers", "Photo sharing websites", "Social networking websites", "Software value added resellers", "System integrators", "User-generated content sites", "Video sharing websites", "Web designers", "Web hosting (Reselling)", "Web-based communities", "Web-casters", "Web service providers"],
-        "Life Sciences": ["Animal Products", "Bio", "Biotics", "Blood bank", "Cannabis", "CBD", "Clinical Trials", "Drugs", "Hemp", "Laboratories", "Medical Devices (MD)", "Nutra", "Pain Relief", "Patients", "Research and Development (R&D)", "Research Trials", "Stem cell bank", "Supplements (food, dietary)", "Therapeutic devices", "Vitamins"],
         "FinTech": ["Digital & mobile banking services", "Digital lending", "Investment platforms", "Money transfer / payment services"]
     };
 
@@ -2312,47 +2311,49 @@ function displayLists(insurance) {
     }
 }
 
-// Policy number checking
-document.getElementById('policyNumberBox').addEventListener('input', function() {
+// Add event listener to the policy number input
+document.getElementById('policyNumber').addEventListener('input', function() {
     const input = this.value.trim().toUpperCase();
-    let result = '';
-
-    if (input.startsWith('GL')) {
-        result = 'Property and Casualty';
-    } else if (input.startsWith('CM')) {
-        result = 'Crisis Management / Product Recall';
-    } else if (input.startsWith('ES')) {
-        result = 'Tech/ Cyber';
-    } else if (input.startsWith('FI')) {
-        result = 'Financial Institutions';
-    } else if (input.startsWith('MS')) {
-        result = 'Healthcare';
-    } else if (input.startsWith('IS')) {
-        result = 'Intellectual Property';
-    } else if (input.startsWith('KR') || input.startsWith('MKR')) {
-        result = 'Kidnap & Ransom';
-    } else if (input.startsWith('BS')) {
-        result = 'Life Science';
-    } else if (input.startsWith('DO')) {
-        result = 'Management Liability';
-    } else if (input.startsWith('TP')) {
-        result = 'Transactional Liability';
-    } else if (input.startsWith('CFTPM')) {
-        result = 'Terrorism';
-    }
-
-    if (input[2] === 'J') {
-        result = 'EXPIRED POLICY';
-        document.getElementById('policyResultBox').style.color = 'red';
-    } else {
-        document.getElementById('policyResultBox').style.color = 'black';
-    }
-
     const policyResultBox = document.getElementById('policyResultBox');
 
-    if (input.length > 0) {
-        policyResultBox.style.display = 'flex';
-        policyResultBox.textContent = result;
+    if (input) {
+        let result = '';
+
+        if (input.startsWith('GL')) {
+            result = 'Property and Casualty';
+        } else if (input.startsWith('CM')) {
+            result = 'Crisis Management / Product Recall';
+        } else if (input.startsWith('ES')) {
+            result = 'Tech / Cyber';
+        } else if (input.startsWith('FI')) {
+            result = 'Financial Institutions';
+        } else if (input.startsWith('MS')) {
+            result = 'Healthcare';
+        } else if (input.startsWith('IS')) {
+            result = 'Intellectual Property';
+        } else if (input.startsWith('KR') || input.startsWith('MKR')) {
+            result = 'Kidnap & Ransom';
+        } else if (input.startsWith('BS')) {
+            result = 'Life Science';
+        } else if (input.startsWith('DO')) {
+            result = 'Management Liability';
+        } else if (input.startsWith('TP')) {
+            result = 'Transactional Liability';
+        } else if (input.startsWith('CFTPM')) {
+            result = 'Terrorism';
+        } else if (input.startsWith('FI')) {
+            result = 'FinTech';
+        }
+
+        if (input[2] === 'J') {
+            result = 'EXPIRED POLICY';
+            policyResultBox.style.color = 'red';
+        } else {
+            policyResultBox.style.color = 'black';
+        }
+
+        policyResultBox.innerHTML = result;
+        policyResultBox.style.display = 'block';
     } else {
         policyResultBox.style.display = 'none';
     }
